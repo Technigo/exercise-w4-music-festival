315d67207bba09a19cb19831a3c2104010a4f84d
Trista <Trista8703@hotmail.com>
2024-02-06 18:56:28 +0200

    the second genre done

--- src/index.js
+++ src/index.js
@@ -3,18 +3,196 @@
 // Start Here
 // Welcome and Introduction
 
+
+
 // Display a welcoming message for the user.
+alert(`Welcome to the Music Festival Concert Slot Reservation! Get ready to choose your favorite music genre.`)
 
 // Prompt the user to enter their name.
+const userName = prompt(`Please enter your name!`)
+
 
 // Display a greeting message with the user's name.
+alert (`Hello, ${userName} ! Let's get started.`)
 
 // Music Genre Selection
 // Ask the user to choose a music genre.
 
 // Initialize a variable to store the selected genre.
+let userSelection = prompt(`Please select a music genre by entering a number
+1. Rock  
+2. Pop 
+3. Electronic`)
 
 // Determine the selected genre based on the user's choice.
+switch (userSelection){
+    case "1":
+        selection= "Rock"
+        alert(`You have chosen ${selection}!`)
+        artistName = prompt(`Please choose the artist by entering number
+        1. Led Zeppelin
+        2. Queen
+        3. The Rolling Stones`)
+        switch(artistName){
+            case "1":
+                artist = "Led Zeppelin"
+                alert(`You have shosen ${artist}!`)
+                timeSlot = timeSlot = prompt(`Please choose your time by entering number
+                1. 07.02.2024, 7pm
+                2. 08.02.2024, 7pm
+                3. 09.02.2024, 7pm`)
+                switch(timeSlot){
+                    case "1":
+                        time = "07.02.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                    case "2":
+                        time = "08.02.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                    case "3":
+                        time = "09.02.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                }
+                break
+            case "2":
+                artist = "Queen"
+                alert(`You have shosen ${artist}!`)
+                timeSlot = prompt(`Please choose your time by entering number
+                1. 10.02.2024, 7pm
+                2. 11.02.2024, 7pm
+                3. 12.02.2024, 7pm`)
+                switch(timeSlot){
+                    case "1":
+                        time = "10.02.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                    case "2":
+                        time = "11.02.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                    case "3":
+                        time = "12.02.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                }
+                break
+            case "3":
+                artist = "The Rolling Stones"
+                alert(`You have shosen ${artist}!`)
+                timeSlot = prompt(`Please choose your time by entering number
+                1. 13.02.2024, 7pm
+                2. 14.02.2024, 7pm
+                3. 14.02.2024, 7pm`)
+                switch(timeSlot){
+                    case "1":
+                        time = "13.02.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                    case "2":
+                        time = "14.02.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                    case "3":
+                        time = "15.02.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                }
+                break
+        }
+        break
+    case "2":
+        selection= "Pop"
+        alert(`You have chosen ${selection}!`)
+        artistName = prompt(`Please choose the artist by entering number
+        1. Michael Jackson
+        2. Madonna
+        3. Taylor Swift`)
+        switch(artistName){
+            case "1":
+                artist = "Michael Jackson"
+                alert(`You have shosen ${artist}!`)
+                timeSlot = prompt(`Please choose your time by entering number
+                1. 07.03.2024, 7pm
+                2. 08.03.2024, 7pm
+                3. 09.03.2024, 7m`)
+                switch(timeSlot){
+                    case "1":
+                        time = "07.03.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                    case "2":
+                        time = "08.03.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                    case "3":
+                        time = "09.03.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                }
+
+                break
+            case "2":
+                artist = "Madonna"
+                alert(`You have shosen ${artist}!`)
+                timeSlot = prompt(`Please choose your time by entering number
+                1. 10.03.2024, 7pm
+                2. 11.03.2024, 7pm
+                3. 12.03.2024, 7m`)
+                switch(timeSlot){
+                    case "1":
+                        time = "10.03.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                    case "2":
+                        time = "11.03.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                    case "3":
+                        time = "12.03.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                }
+
+                break
+            case "3":
+                artist = "Taylor Swift"
+                alert(`You have shosen ${artist}!`)
+                timeSlot = prompt(`Please choose your time by entering number
+                1. 13.03.2024, 7pm
+                2. 14.03.2024, 7pm
+                3. 15.03.2024, 7pm`)
+                switch(timeSlot){
+                    case "1":
+                        time = "13.03.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                    case "2":
+                        time = "14.03.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                    case "3":
+                        time = "15.03.2024, 7pm"
+                        alert(`${time}, your reservation has been confirmed!`)
+                        break
+                }
+                break
+        }
+        break
+    case "3":
+        selection= "Electronic"
+        alert(`You have chosen ${selection}!`)
+        artistName = prompt(`Please choose the artist by entering number
+        1. Daft Punk
+        2.  Calvin Harris
+        3. Aphex Twin`)
+        alert(`You have shosen ${artistName}!`)
+        break
+    default:
+        alert(`Invalid, please enter number.`)    
+}
+
 
 // Display the chosen genre.
 
