2db3a7881213e2d396252b83a5b748f30f275e41
loren <lorenzo.dastoli@gmail.com>
2024-02-09 17:20:56 +0100

    update the whole code

--- src/index.js
+++ src/index.js
@@ -4,20 +4,20 @@
 // Welcome and Introduction
 
 // Display a welcoming message for the user.
-alert("Welcome to the Music Festival Concert Slot Reservation! Get ready to choose your favorite music genre.");
+alert("🎶 Welcome to the Music Festival Concert Slot Reservation! Get ready to choose your favorite music genre.🤘");
 
 // Prompt the user to enter their name.
 // Display a greeting message with the user's name.
-let userName = prompt ("Please enter your name:", "Alfons Åberg");
-alert (`Hello ${userName}!`);
+let userName = prompt ("Please enter your name:", "ex.Alfons Åberg");
+alert (`👋 Hello ${userName}!`);
 
 // Music Genre Selection
 // Ask the user to choose a music genre.
 let = genreChoice = prompt (
   `Please select your favourite music genre:
-  "1"- Rock 
-  "2"- Pop
-  "3"- Electronic
+  "1"- Rock 🎸
+  "2"- Pop 🎤
+  "3"- Electronic 🎧
   `
 );
 
@@ -34,41 +34,45 @@ switch (genreChoice) {
     selectedGenre = "Electronic";
     break;
   default:
-    alert("Invalid choice. Please select a valid number"),
+    alert("Invalid choice. ⚠️ Please select a valid number.");
     exit(1);
 }
 
 // Determine the selected genre based on the user's choice.
 // Display the chosen genre.
-alert (`You´ve choisen ${selectedGenre}!`)
+alert (`You´ve chosen ${selectedGenre}!`)
 
 // Concert Slot Reservation
 // Ask the user to select a concert slot based on the chosen genre.
 let slotChoice;
 switch (selectedGenre) {
   case "Rock":
-    slotChoice = prompt(`Please select a concert slot for ${selectedGenre}:
+    slotChoice = prompt(`📅 Please select a concert slot for ${selectedGenre}:
     1 - Metallica (9:00 PM)
     2 - AC/DC (10:00 PM)
-    3 - Radiohead (11:00 PM)`);
+    3 - Radiohead (11:00 PM)
+    `);
     break;
   case "Pop":
-    slotChoice = prompt(`Please select a concert slot for ${selectedGenre}:
+    slotChoice = prompt(`📅 Please select a concert slot for ${selectedGenre}:
     1 - Beyoncé (9:00 PM)
     2 - The Weekend (10:00 PM)
-    3 - Justin Bieber (11:00 PM)`);
+    3 - Justin Bieber (11:00 PM)
+   `);
     break;
   case "Electronic":
-    slotChoice = prompt(`Please select a concert slot for ${selectedGenre}:
+    slotChoice = prompt(`📅 Please select a concert slot for ${selectedGenre}:
     1 - Massive Attack (9:00 PM)
     2 - Skrillex (10:00 PM)
-    3 - Acid Sister (11:00 PM)`);
+    3 - Acid Sister (11:00 PM)
+    `);
     break;
 }
 
 // Determine the available concert slots and corresponding artist names.
 let selectedSlot;
 let artistName;
+
 // bonus
 // Determine the selected time slot based on the user's concert slot choice.
 switch (slotChoice) {
@@ -130,4 +134,8 @@ switch (selectedGenre) {
 }
 
 // Display a message confirming the user's reservation based on the selected artist, time slot, and genre.
-alert(`Great choice! You've reserved a spot to see ${artistName} at ${selectedSlot} for the ${selectedGenre} concert!`);
+if (confirm("Would you like to proceed with your reservation? 🎫")) {
+alert(`✅Great choice! You've reserved a spot to see ${artistName} at ${selectedSlot} for the ${selectedGenre} concert. Enjoy!🎉`);
+} else {
+  alert("Ok, come back if you change your mind!🙃");
+}
